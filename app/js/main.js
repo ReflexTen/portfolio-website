386024c72eea4968ed2aa2d80ef10d99866c359d
ReflexTen <bulanov.roman4@gmail.com>
2023-03-26 18:48:00 +0500

    added animation

--- app/js/main.js
+++ app/js/main.js
@@ -37,3 +37,19 @@ menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('active-menu')
   navBar.classList.toggle('active')
 })
+
+///////////////////////////////////////////////
+
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+})
+
+ScrollReveal().reveal('.home__content, .heading', { origin: 'top' })
+ScrollReveal().reveal('.home__img, .portfolio__container, .contacts__form', {
+  origin: 'bottom',
+})
+ScrollReveal().reveal('.about__img', { origin: 'left' })
+ScrollReveal().reveal('.about__content', { origin: 'right' })
